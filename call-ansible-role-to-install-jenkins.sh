95380957447ec1008109e73e1f50962053040765
JAGANNATHDEVOPS <154505639+JAGANNATHDEVOPS@users.noreply.github.com>
2024-01-10 00:28:09 +0530

    Update call-ansible-role-to-install-jenkins.sh

--- call-ansible-role-to-install-jenkins.sh
+++ call-ansible-role-to-install-jenkins.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 os_type=$(uname -s)
 # Check if the operating system is Ubuntu
-if [ "$os_type" == "Linux" ]; then
+if [ "$os_type" = "Linux" ]; then
         echo "**********************************"
         echo "IT'S LINUX, BUT OS TYPE IS BELOW"
         if [ -e /etc/os-release ]; then
-                source //etc/os-release
-                if [ "$ID" == "ubuntu" ]; then
+                . /etc/os-release
+                if [ "$ID" = "ubuntu" ]; then
                         echo "======================="
                         echo "IT'S UBUNTU"
                         git clone https://github.com/JAGANNATHDEVOPS/JENKINS-TUTORIAL.git
-                        sudo cp -r JENKINS-TUTORIAL/jenkins-installation /etc/ansible/roles/
                         sudo rm -rf /etc/ansible/roles/jenkins-installation/tasks/main.yml
                         sudo cp JENKINS-TUTORIAL/jenkins-install-in-both-ubuntu-amazon-linux.yml JENKINS-TUTORIAL/jenkins-installation/tasks/main.yml
                         sudo ansible-playbook ./jenkins-install-ubuntu.yml
                         echo "======================="
                         exit 0
-                elif [ "$ID" == "amzn" ]; then
+                elif [ "$ID" = "amzn" ]; then
                         echo "======================="
                         echo "IT'S AMAZON LINUX"
                         echo "======================="
